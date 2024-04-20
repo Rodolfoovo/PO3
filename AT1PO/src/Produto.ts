f9928705b121e303f4db3e991a8529b43cbc396f
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-04-19 22:08:59 -0300

    Mudança nas classes

---
+++ AT1PO/src/Produto.ts
@@ -0,0 +1,63 @@
+export  class Produto{
+
+    private _codigo : number;
+    private _descricao : string;
+    private _valor : number;
+
+
+	constructor(codigo:number, descricao:string, valor:number) {
+        this._codigo = codigo;
+        this._descricao = descricao;
+        this._valor = valor;
+	}
+    
+
+    /**
+     * Getter descricao
+     * @return {string}
+     */
+	public get descricao(): string {
+		return this._descricao;
+	}
+
+    /**
+     * Setter descricao
+     * @param {string} value
+     */
+	public set descricao(value: string) {
+		this._descricao = value;
+	}
+
+    /**
+     * Getter valor
+     * @return {number}
+     */
+	public get valor(): number {
+		return this._valor;
+	}
+
+    /**
+     * Setter valor
+     * @param {number} value
+     */
+	public set valor(value: number) {
+		this._valor = value;
+	}
+
+    /**
+     * Getter codigo
+     * @return {number}
+     */
+	public get codigo(): number {
+		return this._codigo;
+	}
+
+    /**
+     * Setter codigo
+     * @param {number} value
+     */
+	public set codigo(value: number) {
+		this._codigo = value;
+	}
+
+}
---
+++ AT1PO/src/Venda.ts
@@ -0,0 +1,80 @@
+import {Cliente} from "./Cliente"
+import {Produto} from "./Produto"
+class Venda{
+    private _codigo :number;
+    private _data : number;
+    private _cliente : Cliente;
+    private _produtos : Produto[];
+
+
+    /**
+     * Getter produtos
+     * @return {Produto[]}
+     */
+	public get produtos(): Produto[] {
+		return this._produtos;
+	}
+
+    /**
+     * Setter produtos
+     * @param {Produto[]} value
+     */
+	public set produtos(value: Produto[]) {
+		this._produtos = value;
+	}
+
+    /**
+     * Getter codigo
+     * @return {number}
+     */
+	public get codigo(): number {
+		return this._codigo;
+	}
+
+    /**
+     * Getter data
+     * @return {number}
+     */
+	public get data(): number {
+		return this._data;
+	}
+
+    /**
+     * Getter cliente
+     * @return {Cliente}
+     */
+	public get cliente(): Cliente {
+		return this._cliente;
+	}
+
+    /**
+     * Setter codigo
+     * @param {number} value
+     */
+	public set codigo(value: number) {
+		this._codigo = value;
+	}
+
+    /**
+     * Setter data
+     * @param {number} value
+     */
+	public set data(value: number) {
+		this._data = value;
+	}
+
+    /**
+     * Setter cliente
+     * @param {Cliente} value
+     */
+	public set cliente(value: Cliente) {
+		this._cliente = value;
+	}
+    public calculaTotal() : number{
+        let total = 0;
+        for(let i = 0; i < this.produtos.length; i++){
+            total += this.produtos[i].valor;
+        }
+        return total;
+    }
+}
